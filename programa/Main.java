2ac66a0e9f162386397aeecb877834cdf3dc4098
Joao Cosme <joao.pedro.cosme@sap.com>
2022-03-16 08:52:34 -0300

    Criar orquestrador

--- programa/Main.java
+++ programa/Main.java
@@ -1,9 +1,10 @@
 package programa;
 
+import programa.orquestrador.Orquestrador;
 import programa.tela.Interface;
 
 public class Main {
     public static void main(String[] args) {
-        new Interface().abrirTela();
+        Orquestrador.orquestrar();
     }
 }
