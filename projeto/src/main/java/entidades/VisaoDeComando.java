930a377e738a14775b5e5d342bef4c7d4c914d1a
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-04-26 18:58:47 -0300

    Adicionado visaoDeComando

---
+++ projeto/src/main/java/entidades/VisaoDeComando.java
@@ -0,0 +1,28 @@
+package entidades;
+
+import enums.Comando;
+
+public class VisaoDeComando {
+    private Comando comando;
+    private int repeticoes;
+
+    public VisaoDeComando(Comando comando) {
+        this.comando = comando;
+    }
+
+    public Comando getComando() {
+        return comando;
+    }
+
+    public int getRepeticoes() {
+        return repeticoes;
+    }
+
+    public void setRepeticoes(int repeticoes) {
+        this.repeticoes = repeticoes;
+    }
+
+    public void incRepeticao(){
+        this.repeticoes++;
+    }
+}
